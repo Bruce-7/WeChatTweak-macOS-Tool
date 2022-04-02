eb73b1bb438d0c1d0a14abfd6e8556c00902287f
HeDong <hedong2323@foryou56.com>
2022-04-02 21:42:55 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# WeChatTweak-macOS-Tool
+很多人不会使用作者提供的安装方式或遇见各种问题，固对WeChatTweak-macOS工程构建的framework提供另一种安装、卸载方式，仅供学习和研究。
+
+## 安装/卸载插件补丁说明：
+安全说明：
+当前目录下双击执行 install 安装。
+当前目录下双击执行 uninstall 卸载。
+
+如果提示安全问题请：设置-安全性与隐私-通用-任何来源，如果有提示就点击【仍要打开】。
+
+## 更新说明：
+如果作者有更新，自己下载去 https://github.com/Sunnyyoung/WeChatTweak-macOS/releases/ 
+下载 WeChatTweak.framework.zip ，解压后替换 WeChatPatch 目录下的。
+
+## 项目地址：
+[WeChatTweak-macOS](https://github.com/Sunnyyoung/WeChatTweak-macOS)
+
+## 免责说明：
+本项目仅限用于学习和研究目的；不得将上述内容用于商业或者非法用途，否则一切后果请个人或者使用者自负。您必须在下载后的24个小时之内，从您的电脑中彻底删除上述内容。
---
+++ WeChatPatch/WeChatTweak.framework/Versions/A/Headers/WeChatTweak.h
@@ -0,0 +1,23 @@
+//
+//  WeChatTweak.h
+//  WeChatTweak
+//
+//  Created by Sunnyyoung on 2017/8/11.
+//  Copyright © 2017年 Sunnyyoung. All rights reserved.
+//
+
+#import "WeChatTweakHeaders.h"
+
+FOUNDATION_EXPORT double WeChatTweakVersionNumber;
+FOUNDATION_EXPORT const unsigned char WeChatTweakVersionString[];
+
+typedef NS_ENUM(NSUInteger, WTRevokedMessageStyle) {
+    WTRevokedMessageStyleClassic = 0,
+    WTRevokedMessageStyleMask
+};
+
+@interface WeChatTweak : NSObject
+
+@property (nonatomic, assign, class) WTRevokedMessageStyle revokedMessageStyle;
+
+@end
